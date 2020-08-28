aa39b43e9a52a495af0fb3b6fc0909fba91fb509
Alex <gadddina@gmail.com>
2020-08-28 15:41:37 +0300

    Update tooltip position in settings

--- scripts/settingsController.js
+++ scripts/settingsController.js
@@ -65,8 +65,8 @@ function generateSettingCat() {
                 <label class="custom-control-label" for="isCopyToClip">${copyToClipBlockName}</label>
             </div>
         </td>
-        <td class='settingsTooltip'>
-            <div data-toggle="tooltip" title="${copyToClipboardTooltip}">
+        <td>
+            <div class='settingsTooltip' data-toggle="tooltip" title="${copyToClipboardTooltip}">
                 <span>
                     <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-question-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
